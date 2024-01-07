1a743e13a40ca0b4364e615d979e474a4032c702
ShahzadSaeed <shahzadsaeed036@gmail.com>
2024-01-07 23:10:07 +0500

    added basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,3 @@
 class ApplicationController < ActionController::Base
-  http_basic_authenticate_with name: 'admin', password: 'Fr0mD@y1' unless Rails.env.production?
+  http_basic_authenticate_with name: 'admin', password: 'Fr0mD@y1'
 end
